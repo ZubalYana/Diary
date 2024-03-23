553d66a27585b6da698a07cd7c3a1022693a95a4
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-23 19:31:21 +0200

    finish the admin panel dark theme

--- public/admin/main.js
+++ public/admin/main.js
@@ -1,3 +1,4 @@
+//homework adding
 $('.addBtn').click(()=>{
     // let Mondayhomework1 = $('#homework1').val();
     // console.log(Mondayhomework1);
@@ -27,3 +28,57 @@ $('.addBtn').click(()=>{
     axios.post('/addNewTuesdayHomework', TuesdayData)
 
 })
+
+//theme
+let theme = localStorage.getItem('theme') || 'light';
+$('#theme').click(function(){
+
+    if(theme == 'light'){
+        theme = 'dark';
+        localStorage.setItem('theme', theme);
+        changeTheme(theme);
+
+    }else{
+        theme = 'light';
+        localStorage.setItem('theme', theme);
+        changeTheme(theme);
+    }
+})
+
+function changeTheme(theme){
+    if(theme == 'light'){
+        $('#theme').css('justify-content', 'flex-start');
+        $('.wrap').css('background-color', '#fff');
+        $('.wrap').css('color', '#333');
+        $('.weekDay').css('border', '#393E46 3px solid');
+        $('.weekDay_lessonHomework').css('color', '#000');
+        $('.theme').css('background-color', '#393E46');
+        $('.theme_circle').css('background-color', '#fff');
+        $('.theme_circle').css('border', '3px solid #393E46');
+        $('input').css('background-color', '#fff')
+        $('.weekDay_lessonName').css('color', '#333');
+        $('.weekDay_heading').css('color', '#333');
+        $('h1').css('color', '#333');
+        $('.addBtn').css('border', '#333 3px solid');
+        $('.addBtn').css('color', '#333');
+
+
+    }else{
+        $('#theme').css('justify-content', 'flex-end');
+        $('.wrap').css('background-color', '#393E46');
+        $('.wrap').css('color', '#fff');
+        $('.weekDay').css('border', '#eee 3px solid');
+        $('.weekDay_lessonHomework').css('color', '#fff');
+        $('.theme').css('background-color', '#fff');
+        $('.theme_circle').css('background-color', '#393E46');
+        $('.theme_circle').css('border', '3px solid #fff');
+        $('input').css('background-color', '#393E46')
+        $('.weekDay_lessonName').css('color', '#fff');
+        $('.weekDay_heading').css('color', '#fff');
+        $('h1').css('color', '#fff');
+        $('.addBtn').css('border', '#fff 3px solid');
+        $('.addBtn').css('color', '#fff');
+
+    }
+}
+changeTheme(theme);
