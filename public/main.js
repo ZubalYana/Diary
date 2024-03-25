3fce995f33aa4fa94d9d0c0b8fe447fb68f3399a
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-25 18:51:42 +0200

    fix the weekends date

--- public/main.js
+++ public/main.js
@@ -37,7 +37,7 @@ function countdownNextHolidays() {
 setInterval(countdownNextHolidays, 1000);
 function countdownWeekends() {
     var currentDate = new Date(); 
-    var targetDate = new Date("March 23, 2024 00:00:00"); 
+    var targetDate = new Date("March 30, 2024 00:00:00"); 
 
     var difference = targetDate.getTime() - currentDate.getTime();
 
